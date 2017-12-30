aae57369dd379c55be6fd431359286d124c83aca
Roman <rbolkhovitin@gmail.com>
2017-12-30 20:58:01 +0300

    add uglify plugin in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const extractLess = new ExtractTextPlugin({
   filename: "styles.css",
@@ -41,6 +42,7 @@ module.exports = {
     'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
   },
   plugins: [
-    extractLess
+    extractLess,
+    new UglifyJSPlugin()
   ]
 };
