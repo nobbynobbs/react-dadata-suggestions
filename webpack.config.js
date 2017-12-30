021fdbdc45001dc9ae7cc85f4f3070ddd64bea49
Roman <rbolkhovitin@gmail.com>
2017-12-30 15:51:06 +0300

    add babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const extractLess = new ExtractTextPlugin({
 });
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
