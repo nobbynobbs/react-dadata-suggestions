ff974d51eb396726cb49d755515d4784c2db3dd2
Roman <rbolkhovitin@gmail.com>
2018-01-03 18:33:39 +0300

    refactor keyboard handlers

--- src/handlers/handleKeyPress.js
+++ src/handlers/handleKeyPress.js
@@ -1,3 +1,11 @@
+import {
+  handleEnter,
+  handleTab,
+  handleEscape,
+  handleArrowUp,
+  handleArrowDown,
+} from "./index";
+
 function handleKeyPress(e) {
 
   if (e.shiftKey || e.ctrlKey || e.altKey) {
@@ -7,30 +15,23 @@ function handleKeyPress(e) {
   const arrowDownKey = 40, arrowUpKey = 38, enterKey = 13, escapeKey = 27, tabKey = 9;
 
   if ([arrowDownKey, arrowUpKey, enterKey, escapeKey, tabKey].includes(e.which)) {
-    e.preventDefault();
 
-    const { selected, suggestions } = this.state;
-    const maxSuggestionIndex = suggestions.length - 1;
-
-    if (maxSuggestionIndex === -1) {
-      return;
-    }
-
-    if (e.which === arrowUpKey) {
-      this.setState({
-        selected: selected > 0 ? selected - 1 : maxSuggestionIndex
-      });
-    }
-    if (e.which === arrowDownKey) {
-      this.setState({
-        selected: selected < maxSuggestionIndex ? selected + 1 : 0
-      });
-    }
-    if ((e.which === enterKey || e.which === tabKey) && selected !== -1) {
-      this.handleSelect(selected)();
-    }
-    if (e.which === escapeKey) {
-      this.makeListInvisible();
+    switch (e.which) {
+      case arrowUpKey:
+        handleArrowUp(e, this);
+        break;
+      case arrowDownKey:
+        handleArrowDown(e, this);
+        break;
+      case enterKey:
+        handleEnter(e, this);
+        break;
+      case tabKey:
+        handleTab(e, this);
+        break;
+      case escapeKey:
+        handleEscape(e, this);
+        break;
     }
   }
 }
